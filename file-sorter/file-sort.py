afe8dae9effe5ac5f780cd594f02235c5e861d7e
lolcatz1991 <lasselthorsen@gmail.com>
2024-01-22 18:38:16 +0100

    Update file-sort.py

--- file-sorter/file-sort.py
+++ file-sorter/file-sort.py
@@ -11,40 +11,42 @@ def folder_maker(directory):
         os.makedirs(directory)
 
 
+chosen_folder = input("Paste the folder you want to clean here ( full path ):")
+
 # images folder
-directory_images = "G:\My Drive\Downloads\images"
+directory_images = f"{chosen_folder}\images"
 folder_maker(directory_images)
 # pdf folder
-directory_pdf = "G:\My Drive\Downloads\pdf"
+directory_pdf = f"{chosen_folder}\pdf"
 folder_maker(directory_pdf)
 # misc folder
-directory_misc = "G:\My Drive\Downloads\misc"
+directory_misc = f"{chosen_folder}\misc"
 folder_maker(directory_misc)
 
 # Get the list of files in the folder
-file_list = os.listdir("G:\My Drive\Downloads")
+file_list = os.listdir(f"{chosen_folder}")
 
 # Arrange the list of files into the folders
 for file_name in file_list:
     # print(file_name)
     match file_name[-4:]:
         case ".svg":
-            shutil.move(f"G:\My Drive\Downloads\{file_name}", directory_images)
+            shutil.move(f"{chosen_folder}\{file_name}", directory_images)
 
         case ".png":
-            shutil.move(f"G:\My Drive\Downloads\{file_name}", directory_images)
+            shutil.move(f"{chosen_folder}\{file_name}", directory_images)
 
         case ".jpg":
-            shutil.move(f"G:\My Drive\Downloads\{file_name}", directory_images)
+            shutil.move(f"{chosen_folder}\{file_name}", directory_images)
 
         case "jpeg":
-            shutil.move(f"G:\My Drive\Downloads\{file_name}", directory_images)
+            shutil.move(f"{chosen_folder}\{file_name}", directory_images)
 
         case ".pdf":
-            shutil.move(f"G:\My Drive\Downloads\{file_name}", directory_pdf)
+            shutil.move(f"{chosen_folder}\{file_name}", directory_pdf)
 
         case ".exe":
-            shutil.move(f"G:\My Drive\Downloads\{file_name}", directory_misc)
+            shutil.move(f"{chosen_folder}\{file_name}", directory_misc)
 
         case _:
             print(f"{file_name} does not fit in a folder yet...")
